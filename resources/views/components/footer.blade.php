00626cfc9ce1a3eefa3830a0788fed040664e66e
Eddy - 小茗 <vincent7697@gmail.com>
2019-04-11 03:38:29 +0800

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,104 @@
+@php
+    $language = '';
+
+    foreach (Config::get('languages') as $key => $value) {
+        if ($key == App::getLocale()) {
+            $language = $value;
+        }
+    }
+@endphp
+
+<section class="site-footer" data-aos="fade-up">
+    <div class="container-fluid site-footer-content">
+        {{-- footer-section --}}
+        <div class="col-md-12 footer-section">
+
+            {{-- footer-info --}}
+            <div class="row footer-info">
+                <div class="col-md-4 site-info">
+                    <div class="footer-logo">
+                        <img src="/img/site-logo/logo_footer.png" alt="">
+                    </div>
+                    <ul class="contact-info">
+                        <li>地址：中国 上海市松江区车新公路518号</li>
+                        <li>电话：+86-21-5760-9598</li>
+                        <li>传真：+86-21-5760-9698</li>
+                        <li>网址：www.grapeking.com.cn</li>
+                        <li>电邮：gki@grapeking.com.cn</li>
+                    </ul>
+                </div>
+                <div class="col-md-4 icp">
+                    <div class="icp-card">
+                        <img class="icp-icon" src="/img/site-logo/icp.png" alt="">
+                        <span>沪ICP备13010123号-1</span>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-6 site-map">
+                            <h5>关于上海葡萄王</h5>
+                            <ul>
+                                <li>
+                                    <a href="">企业愿景</a>
+                                </li>
+                                <li>
+                                    <a href="">发展历程</a>
+                                </li>
+                                <li>
+                                    <a href="">关于创办人</a>
+                                </li>
+                                <li>
+                                    <a href="">安心认证</a>
+                                </li>
+                                <li>
+                                    <a href="">公司据点</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 site-map">
+                            <h5>专业研发</h5>
+                            <ul>
+                                <li>
+                                    <a href="">食安中心</a>
+                                </li>
+                                <li>
+                                    <a href="">生物工程中心</a>
+                                </li>
+                                <li>
+                                    <a href="">创新研发中心</a>
+                                </li>
+                                <li>
+                                    <a href="">专利及文献发表</a>
+                                </li>
+                                <li>
+                                    <a href="">世界级工厂</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- footer-info --}}
+        </div> 
+        {{-- footer-section --}}
+
+    </div>
+</section>
+<section class="site-copyright"  data-aos="fade-up">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-11 mx-auto">
+                <div class="row">
+                    <div class="col-md-6 info" style="">
+                        Copyright &copy; Shanghai Grape King Enterprises Corp.
+                    </div>
+                    <div class="col-md-6 info" style="text-align: right">
+                        <a href="">隐私权政策</a>&nbsp;|&nbsp;<a href="">使用条款</a>
+                    </div> 
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
