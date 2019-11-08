5d55905376a352f3b4bfa996cad711e8e8b1e909
Eddy <vincent7697@gmail.com>
2019-11-08 22:38:08 +0800

    timeline

--- resources/views/frontend/about/course.blade.php
+++ resources/views/frontend/about/course.blade.php
@@ -7,122 +7,107 @@
 @section('custom-script')
 <script src="/js/plugins/perfect-scrollbar-master/dist/perfect-scrollbar.min.js"></script>
 <script>
-// new PerfectScrollbar('.thumb-container');
+    var windowWidth = $(window).width()
 
-// updatePosition();
-updateLayout();
+    $('timeline').attr('data-aos', 'fade-down');
 
-// $(window).scroll(function () {
-//     updatePosition();
-// });
-
-$(window).resize(function () {
-    // updatePosition();
-    updateLayout();
-});
-
-// function updatePosition() {
-//     var windowScrollTop = $(window).scrollTop();
-//     var headerHeight = $('#site-header-bar').height() + $('.sub-page-banner').height();
-//     var menuHeight = $('.site-header').height();
-//     var elmHeight = $(window).scrollTop() < $('.vdieo-present-section').offset().top - $(window).height() ? $(window).height() - $('.site-header').height() : $('.vdieo-present-section').offset().top - $(window).scrollTop() - $('.site-header').height()
-
-//     if (windowScrollTop > headerHeight - 140) {
-//         $('.thumb-container').css({
-//             'position': 'fixed',
-//             'top': (menuHeight - 32) + 'px',
-//             'left': $('.about-thumbnail').offset().left + 32,
-//             'height': elmHeight
-//         });
-//     } else {
-//         $('.thumb-container').css({
-//             'position': 'initial',
-//             'top': 'initial',
-//             'left': 'initial',
-//             'height': 'initial'
-//         });
-//     }
-// }
-
-function updateLayout() {
-    var windowWidth = $(window).width();
-
-    if (windowWidth > 768) {
-        $('#course-container').attr('class', 'container');
-    } else if (windowWidth <= 768) {
-        $('#course-container').attr('class', 'container max-xs-size');
+    if (windowWidth < 768) {
+        $('.container-timeline:nth-child(odd)').attr('data-aos', 'fade-left');
+        $('.container-timeline:nth-child(even)').attr('data-aos', 'fade-left');
+    } else {
+        $('.container-timeline:nth-child(odd)').attr('data-aos', 'fade-right');
+        $('.container-timeline:nth-child(even)').attr('data-aos', 'fade-left');
     }
-}
+    setTimeout(function () {
+        AOS.init();
+    }, 500);
 </script>
-<script>
-var s = $("html"),
-    e = $("body"),
-    n = ($(".fit"), $(".imgfull"), $(".slick-slide")),
-    i = $(".slider-nav"),
-    a = $(".slider-for"),
-    t = $(".date input"),
-    r = !1;
+@if (false)
+    <script>
+        updateLayout();
+        
+        $(window).resize(function () {
+            updateLayout();
+        });
+        
+        function updateLayout() {
+            var windowWidth = $(window).width();
+        
+            if (windowWidth > 768) {
+                $('#course-container').attr('class', 'container');
+            } else if (windowWidth <= 768) {
+                $('#course-container').attr('class', 'container max-xs-size');
+            }
+        }
+        </script>
+        <script>
+        var s = $("html"),
+            e = $("body"),
+            n = ($(".fit"), $(".imgfull"), $(".slick-slide")),
+            i = $(".slider-nav"),
+            a = $(".slider-for"),
+            t = $(".date input"),
+            r = !1;
+        
+            i.slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                asNavFor: ".slider-for",
+                focusOnSelect: !0,
+                infinite: !0,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                responsive: [{
+                    breakpoint: 1530,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }, {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }, {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }, {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }]
+            }), i.on("afterChange", function (o, s, e, n) {
+                var i = parseInt($(".slick-slide:first-child").attr("data-slick-index")),
+                    a = parseInt($(".slick-slide:last-child").attr("data-slick-index")),
+                    t = parseInt($(s.$slides.get(e)).attr("data-slick-index"));
+                t == i ? $(this).addClass("firstIndex") : $(this).removeClass("firstIndex"), t == a ? $(this).addClass("lastIndex") : $(this).removeClass("lastIndex")
+            }), a.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade: !0,
+                asNavFor: ".slider-nav",
+                // adaptiveHeight: true,
+                infinite: !0,
+                arrows: !1
+            }), $('a[data-toggle="tab"]').on("shown.bs.tab", function () {
+                $(".slider").slick("setPosition")
+            })
+        $(".slider-nav").on('init', function () {
+            $(this).show() 
+        });
+        $(".slider-for").on('beforeChange', function (event, slick, direction) {
+        
+            $('.timeline-text-container').removeClass('animated fadeInLeft').hide();
+            $('.timeline-text-container').addClass('animated fadeInLeft').show();
+            
+            $('.feature-image-container .feature-image').removeClass('animated fadeInRight').hide();
+            $('.feature-image-container .feature-image').addClass('animated fadeInRight').show();
+        });
+    </script>
+@endif
 
-    i.slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: ".slider-for",
-        focusOnSelect: !0,
-        infinite: !0,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [{
-            breakpoint: 1530,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    }), i.on("afterChange", function (o, s, e, n) {
-        var i = parseInt($(".slick-slide:first-child").attr("data-slick-index")),
-            a = parseInt($(".slick-slide:last-child").attr("data-slick-index")),
-            t = parseInt($(s.$slides.get(e)).attr("data-slick-index"));
-        t == i ? $(this).addClass("firstIndex") : $(this).removeClass("firstIndex"), t == a ? $(this).addClass("lastIndex") : $(this).removeClass("lastIndex")
-    }), a.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: !0,
-        asNavFor: ".slider-nav",
-        // adaptiveHeight: true,
-        infinite: !0,
-        arrows: !1
-    }), $('a[data-toggle="tab"]').on("shown.bs.tab", function () {
-        $(".slider").slick("setPosition")
-    })
-$(".slider-nav").on('init', function () {
-    $(this).show() 
-});
-$(".slider-for").on('beforeChange', function (event, slick, direction) {
-
-    $('.timeline-text-container').removeClass('animated fadeInLeft').hide();
-    $('.timeline-text-container').addClass('animated fadeInLeft').show();
-    
-    $('.feature-image-container .feature-image').removeClass('animated fadeInRight').hide();
-    $('.feature-image-container .feature-image').addClass('animated fadeInRight').show();
-    // setTimeout(function () {
-    //     $('.timeline-text-container').addClass('animated fadeInLeft').show();
-    // }, 1);
-});
-</script>
 @endsection
 
 @section('custom-style')
@@ -178,60 +163,83 @@ $(".slider-for").on('beforeChange', function (event, slick, direction) {
 <div class="container" id="course-container">
     <div class="row">
         <div class="col-md-12 about-content wp history">
-            <h3 class="about-section-title" id="about_5">{{ trans('string.about11') }}</h3>
-            <div class="about-text">
-                {!! trans('string.about12') !!}
+            
+            <div class="row">
+                <div class="col-md-8">
+                    <h3 class="about-section-title" id="about_5">{{ trans('string.about11') }}</h3>
+                    <div class="about-text force-center-left" style="text-indent : 36px">
+                        {!! trans('string.about12') !!}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img class="about-image" style="max-width: 400px; width: 100%" src="/img/about/pic.png" alt="">
+                </div>
             </div>
-            <img class="about-image" style="max-width: 400px; width: 100%" src="/img/about/pic.png" alt="">
+            
+            
             <h3 class="about-section-title" id="about_1">{{ trans('string.about_1') }}</h3>
-            <div class="content" id="timeline-content" style="margin-bottom: 50px;">
-                <div class="years">
-                    <div class="slider">
-                        <div class="dots slider-nav firstIndex">
-                            @foreach ($timeline as $item)
-                                <div class="dot">
-                                    <p class="year">{{$item->year}}</p>
-                                    <span class="circle"></span>
-                                </div>
-                            @endforeach
+            <div class="timeline">
+                @foreach ($timeline as $item)
+                    <div class="container-timeline">
+                        <div class="content-timeline">
+                            <h2>{{$item->year}}</h2>
+                            <p>{!! $item->content->{App::getLocale()} !!}</p>
+                            <img src="{!! $item->featureImage->{App::getLocale()} !!}" class="feature-image" alt="">
                         </div>
                     </div>
-                    <div class="slider-for lists">
-                        @foreach ($timeline as $item)
-                            <div class="list">
-                                <div class="text-mask">
+                @endforeach
+            </div>
+            @if (false)
+            <div class="content" id="timeline-content" style="margin-bottom: 50px;">
+                    <div class="years">
+                        <div class="slider">
+                            <div class="dots slider-nav firstIndex">
+                                @foreach ($timeline as $item)
+                                    <div class="dot">
+                                        <p class="year">{{$item->year}}</p>
+                                        <span class="circle"></span>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="slider-for lists">
+                            @foreach ($timeline as $item)
+                                <div class="list">
+                                    <div class="text-mask">
+                                        <div class="row">
+                                            <div class="col-xs-12 col-lg-7 text">
+                                                <h4 class="year">{{$item->year}}</h4>
+                                                <ul>
+                                                    <li>{!! $item->content->{App::getLocale()} !!}</li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-xs-12 col-lg-5 image">
+                                                @if (isset($item->featureImage))
+                                                    <img src="{!! $item->featureImage->{App::getLocale()} !!}" class="feature-image" alt="">
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="row">
-                                        <div class="col-xs-12 col-lg-7 text">
+                                        <div class="col-xs-12 col-lg-7 left timeline-text-container">
                                             <h4 class="year">{{$item->year}}</h4>
                                             <ul>
                                                 <li>{!! $item->content->{App::getLocale()} !!}</li>
                                             </ul>
                                         </div>
-                                        <div class="col-xs-12 col-lg-5 image">
+                                        <div class="col-xs-12 col-lg-5 right feature-image-container">
                                             @if (isset($item->featureImage))
                                                 <img src="{!! $item->featureImage->{App::getLocale()} !!}" class="feature-image" alt="">
                                             @endif
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-xs-12 col-lg-7 left timeline-text-container">
-                                        <h4 class="year">{{$item->year}}</h4>
-                                        <ul>
-                                            <li>{!! $item->content->{App::getLocale()} !!}</li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-xs-12 col-lg-5 right feature-image-container">
-                                        @if (isset($item->featureImage))
-                                            <img src="{!! $item->featureImage->{App::getLocale()} !!}" class="feature-image" alt="">
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
+            
             
         </div>
     </div>
